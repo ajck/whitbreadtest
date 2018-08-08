e8af74ea6600e4de3a9b9f7639cb218d64d6dbad
Alex <ajckerr@gmail.com>
2018-08-08 16:40:01 +0100

    AJAX JSON parsing and results generation code initially complete
    Still to be tested and bugfixed.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// If search button is pressed, trigger an AJAX request to the backend, to retrieve results from the Foursquare API:
 $('#searchbtn').click(function(e)
 {
 if($('#alert').hasClass('nodisplay')) $('#alert').addClass('nodisplay'); // Hide alert if still showing from previous search
@@ -8,9 +9,14 @@ $.ajax({
 	dataType: 'json',
 	success: function (data) {
 console.log(data); // FOR DEBUGGING
+
+		$('#results').empty(); // Remove any previous results from the results div
+
 		if(data.err) { // If there was a warning message within the Foursquare results
 			$('#alert').removeClass('nodisplay').html(data.msg); // Activate the Bootstrap alert component and inject the message text
 		}
+		
+		// Generate and inject the HTML for the venue results:
 		if(data.results) { // If there were results to process
 			row_items = 0; // Item counter so we only inject 3 items per row
 			$.each(data.results, function(index, result) { // Parse through results
@@ -18,10 +24,13 @@ console.log(data); // FOR DEBUGGING
 				
 				if(row_items == 0) insert_str = '<div class="row">'; // Start a new row if row item counter indicates it's at the start
 				insert_str += '<div class="col-md-4">'; // Start a new item
-				
+				insert_str += result.name; // Insert venue name
+				insert_str += '<div>' + result.address + '</div>'; // Insert venue address
+				if(result.hasOwnProperty('catname')) insert_str += '<div>' + result.catname + '</div>'; // Insert venue category name, if exists
+				if(result.hasOwnProperty('icon')) insert_str += '<div>' + result.icon + '</div>'; // Insert venue category icon, if exists
 				insert_str += '</div>'; // Terminate new item
-				row_items++;
-				if(row_items == 3) {
+				row_items++; // Increment row item counter
+				if(row_items == 3) { // We've inserted 3 items into a row, so start a new one
 					row_items = 0; // Reset row item counter
 					insert_str += '</div>'; // Terminate a row
 				}
